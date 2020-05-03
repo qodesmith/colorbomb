bf4b15c79b59cf2924d07b0a2324b1fb2e1d7fa6
Aaron Cordova <qodesmith@fb.com>
2020-05-03 17:53:18 -0400

    Sandbox update - showing all 3 color types.

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -5,9 +5,38 @@ import './sandbox.css'
 // Let's us play with colorbomb in the console.
 window.colorbomb = colorbomb
 
-document.querySelector('.colorbomb button').addEventListener('click', () => {
-  const inputValue = document.querySelector('.colorbomb input').value
-  const swatch = document.querySelector('.colorbomb .swatch')
 
-  swatch.style.backgroundColor = colorbomb(inputValue).rawStringValues.rgb
+document.querySelector('.colorbomb form').addEventListener('submit', e => {
+  e.preventDefault()
+  const input = document.querySelector('.colorbomb input')
+  const errorNode = document.querySelector('.colorbomb .error')
+
+  const swatchHex = document.querySelector('.colorbomb .hex')
+  const swatchRgb = document.querySelector('.colorbomb .rgb')
+  const swatchHsl = document.querySelector('.colorbomb .hsl')
+
+  const swatchHexValue = document.querySelector('.colorbomb .hex .value')
+  const swatchRgbValue = document.querySelector('.colorbomb .rgb .value')
+  const swatchHslValue = document.querySelector('.colorbomb .hsl .value')
+
+  if (!input.value) return
+  
+  try {
+    const colorObj = colorbomb(input.value)
+    console.log(colorObj)
+    const alphaOrNot = colorObj.a === 1 ? '' : 'a'
+
+    swatchHexValue.textContent = colorObj.rawStringValues[`hex${alphaOrNot}`]
+    swatchRgbValue.textContent = colorObj.rawStringValues[`rgb${alphaOrNot}`]
+    swatchHslValue.textContent = colorObj.rawStringValues[`hsl${alphaOrNot}`]
+
+    swatchHex.setAttribute('style', `background:${swatchHexValue.textContent}`)
+    swatchRgb.setAttribute('style', `background:${swatchRgbValue.textContent}`)
+    swatchHsl.setAttribute('style', `background:${swatchHslValue.textContent}`)
+
+    input.value = ''
+    errorNode.textContent = ''
+  } catch({ message }) {
+    errorNode.textContent = message
+  }
 })
