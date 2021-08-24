f239fa0f3a9d04aefae2c01cc121d6e26d08eb3f
Qodesmith <theqodesmith@gmail.com>
2021-08-24 09:15:36 -0400

    Updated webpack config to match newer version.

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,63 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
-module.exports = (env, argv) => ({
-  mode: env.prod ? 'production' : 'development',
-  entry: path.resolve(
-    __dirname,
-    env.prod ? 'src/colorbomb.js' : 'sandbox/sandbox.js',
-  ),
-  target: 'web',
-  output: env.prod
-    ? {
-        path: path.resolve(__dirname, 'dist'),
-        library: 'colorbomb',
-        libraryExport: 'default',
-        libraryTarget: 'umd',
-        filename: 'colorbomb.js',
-      }
-    : {},
-  devServer: {
-    open: true,
-    contentBase: path.resolve(__dirname, './sandbox'),
-    port: 9001,
-    public: 'http://localhost:9001',
-  },
-  optimization: {
-    minimize: !!env.prod,
-    minimizer: [
-      new TerserPlugin({
-        parallel: true,
-        terserOptions: {
-          ecma: 5,
-          output: {
-            comments: false,
+export default function webpackConfig(env, argv) {
+  return {
+    mode: env.prod ? 'production' : 'development',
+    entry: path.resolve(
+      path.resolve(),
+      env.prod ? 'src/colorbomb.js' : 'sandbox/sandbox.js',
+    ),
+    target: 'web',
+    output: env.prod
+      ? {
+          path: path.resolve(path.resolve(), 'dist'),
+          library: 'colorbomb',
+          libraryExport: 'default',
+          libraryTarget: 'umd',
+          filename: 'colorbomb.js',
+        }
+      : {},
+    devServer: {
+      open: true,
+      // static: {
+      //   contentBase: path.resolve(path.resolve(), './sandbox'),
+      //   publicPath: 'http://localhost:9001',
+      // },
+      port: 9001,
+    },
+    optimization: {
+      minimize: !!env.prod,
+      minimizer: [
+        new TerserPlugin({
+          parallel: true,
+          terserOptions: {
+            ecma: 5,
+            output: {
+              comments: false,
+            },
           },
-        },
-      }),
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-    ],
-  },
-  plugins: !env.prod
-    ? [
-        new MiniCssExtractPlugin({filename: 'colorbomb.css'}),
-        new HtmlWebpackPlugin({
-          template: path.resolve(__dirname, 'sandbox/index.ejs'),
-          title: 'Color Bomb Sandbox',
         }),
-      ]
-    : [],
-})
+      ],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+      ],
+    },
+    plugins: !env.prod
+      ? [
+          new MiniCssExtractPlugin({filename: 'colorbomb.css'}),
+          new HtmlWebpackPlugin({
+            template: path.resolve(path.resolve(), 'sandbox/index.ejs'),
+            title: 'Color Bomb Sandbox',
+          }),
+        ]
+      : [],
+  }
+}
